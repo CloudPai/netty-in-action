6a8a9d23a3fbfa29659ff491ca4731ed4d5d8cf1
cloudPai <1565730756@qq.com>
2018-08-02 22:47:01 +0800

    添加第9~10章注释2018年08月02日22:46:55

--- chapter10/src/main/java/nia/chapter10/CombinedByteCharCodec.java
+++ chapter10/src/main/java/nia/chapter10/CombinedByteCharCodec.java
@@ -7,10 +7,12 @@ import io.netty.channel.CombinedChannelDuplexHandler;
  *
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
-
+//代码清单 10-10 CombinedChannelDuplexHandler <I,0>
+    //通过该解码器和编码器实现参数化 CombinedByteCharCodec
 public class CombinedByteCharCodec extends
     CombinedChannelDuplexHandler<ByteToCharDecoder, CharToByteEncoder> {
     public CombinedByteCharCodec() {
+        //将委托实例传递给父类
         super(new ByteToCharDecoder(), new CharToByteEncoder());
     }
 }
